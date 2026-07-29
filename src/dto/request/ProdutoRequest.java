e9e4f63a1bd7e307a3f26a8fe18bc948ed470511
Vinícius F. <fonsecavinicius117@gmail.com>
2026-07-28 22:00:40 -0300

    feat: criação do DTO ProdutoRequest para representar a os ddados que entram no banco de dados

---
+++ src/dto/request/ProdutoRequest.java
@@ -0,0 +1,40 @@
+package dto.request;
+
+public class ProdutoRequest {
+    private String nome;
+    private Double preco;
+    private Integer estoque;
+
+    ProdutoRequest() {
+    }
+
+    ProdutoRequest(String nome, Double preco, Integer estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
